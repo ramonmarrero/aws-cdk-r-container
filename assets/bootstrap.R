3200f53bf5bd296b6beff0be78216f8b1a1a713d
ramonmarrero <ramon.marrero@lanube.io>
2021-12-08 17:46:15 +0100

    Fix for bootstrap.R

--- assets/bootstrap.R
+++ assets/bootstrap.R
@@ -1,11 +1,7 @@
 source('/var/task/runtime.R')
 tryCatch({
     function_name <- initializeRuntime()
-    while (TRUE) {
-        handle_request(function_name)
-        logReset()
-        rm(list=ls())
-        source('/var/task/runtime.R')
-        function_name <- initializeRuntime()
-    }
+    handle_request(function_name)
+    logReset()
+    rm(list=ls())
 }, error = throwInitError)
